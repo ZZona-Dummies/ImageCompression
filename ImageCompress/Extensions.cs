0510a8e7a055cdc2c44903ee2c4e96adb3aba8f9
Álvaro <ikillnukes1@gmail.com>
2018-02-06 11:43:41 +0100

    Removed uneeded code

--- ImageCompress/Extensions.cs
+++ ImageCompress/Extensions.cs
@@ -194,7 +194,7 @@ namespace ImageCompress
             }
         }
 
-        public static Bitmap SafeCompare(Bitmap bmp1, Bitmap bmp2)
+        public static Bitmap SafeCompare(Bitmap bmp1, Bitmap bmp2, bool trim)
         {
             if ((bmp1 == null) != (bmp2 == null)) throw new Exception("Null bitmap passed!");
             if (bmp1.Size != bmp2.Size) throw new Exception("Different sizes between bitmap A & B!");
@@ -206,7 +206,6 @@ namespace ImageCompress
                 lockBitmap1.LockBits();
                 lockBitmap2.LockBits();
 
-                Color compareClr = Color.FromArgb(255, 255, 255, 255);
                 for (int y = 0; y < lockBitmap1.Height; y++)
                     for (int x = 0; x < lockBitmap2.Width; x++)
                         if (lockBitmap1.GetPixel(x, y) == lockBitmap2.GetPixel(x, y))
@@ -290,102 +289,9 @@ namespace ImageCompress
             Bitmap dest = new Bitmap(srcRect.Width, srcRect.Height);
             Rectangle destRect = new Rectangle(0, 0, srcRect.Width, srcRect.Height);
             using (Graphics graphics = Graphics.FromImage(dest))
-            {
                 graphics.DrawImage(source, destRect, srcRect, GraphicsUnit.Pixel);
-            }
+
             return dest;
-        }
-
-        public static Bitmap Crop(this Bitmap bmp)
-        {
-            int w = bmp.Width;
-            int h = bmp.Height;
-
-            Func<int, bool> allWhiteRow = row =>
-            {
-                for (int i = 0; i < w; ++i)
-                    if (bmp.GetPixel(i, row).A == 0)
-                        return false;
-                return true;
-            };
-
-            Func<int, bool> allWhiteColumn = col =>
-            {
-                for (int i = 0; i < h; ++i)
-                    if (bmp.GetPixel(col, i).A == 0)
-                        return false;
-                return true;
-            };
-
-            int topmost = 0;
-            for (int row = 0; row < h; ++row)
-            {
-                if (allWhiteRow(row))
-                    topmost = row;
-                else break;
-            }
-
-            int bottommost = 0;
-            for (int row = h - 1; row >= 0; --row)
-            {
-                if (allWhiteRow(row))
-                    bottommost = row;
-                else break;
-            }
-
-            int leftmost = 0, rightmost = 0;
-            for (int col = 0; col < w; ++col)
-            {
-                if (allWhiteColumn(col))
-                    leftmost = col;
-                else
-                    break;
-            }
-
-            for (int col = w - 1; col >= 0; --col)
-            {
-                if (allWhiteColumn(col))
-                    rightmost = col;
-                else
-                    break;
-            }
-
-            if (rightmost == 0) rightmost = w; // As reached left
-            if (bottommost == 0) bottommost = h; // As reached top.
-
-            int croppedWidth = rightmost - leftmost;
-            int croppedHeight = bottommost - topmost;
-
-            if (croppedWidth == 0) // No border on left or right
-            {
-                leftmost = 0;
-                croppedWidth = w;
-            }
-
-            if (croppedHeight == 0) // No border on top or bottom
-            {
-                topmost = 0;
-                croppedHeight = h;
-            }
-
-            try
-            {
-                var target = new Bitmap(croppedWidth, croppedHeight);
-                using (Graphics g = Graphics.FromImage(target))
-                {
-                    g.DrawImage(bmp,
-                      new RectangleF(0, 0, croppedWidth, croppedHeight),
-                      new RectangleF(leftmost, topmost, croppedWidth, croppedHeight),
-                      GraphicsUnit.Pixel);
-                }
-                return target;
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(
-                  string.Format("Values are topmost={0} btm={1} left={2} right={3} croppedWidth={4} croppedHeight={5}", topmost, bottommost, leftmost, rightmost, croppedWidth, croppedHeight),
-                  ex);
-            }
         }
 
         private static IEnumerable<Color> SolveVertical(Bitmap bmp2, int x, int y)
